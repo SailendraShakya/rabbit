18aa758b86a70089a2c1fca5e85be0d7eca86eeb
Sailendra Shakya <sailendrashakya@Sailendras-MacBook-Pro.local>
2017-11-05 12:48:53 +0545

    url fixes

--- public/js/main.js
+++ public/js/main.js
@@ -31,7 +31,7 @@ function initMap() {
 function getTweets(latLng) {
   clearMarkers();
   $.ajax({
-    url: '//rabbit-map.herokuapp.com/map/tweets',
+    url: 'http://rabbit-map.herokuapp.com/map/tweets',
     method: "post",
     data: latLng
   })
@@ -130,7 +130,7 @@ $(document).ready(function(){
   $('#search_history').on('click',function(e){
     e.preventDefault();
     $.ajax({
-      url: '//rabbit-map.herokuapp.com/map/tweets_history',
+      url: 'http://rabbit-map.herokuapp.com/map/tweets_history',
       method: "get"
     })
     .done(function(response) {
